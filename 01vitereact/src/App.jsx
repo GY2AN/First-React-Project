8b5095b2324b05958238861a89cbcde11e4e8493
Gyan <pgyan4224@gmail.com>
2023-10-16 06:12:00 +0530

    Created react with vite

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,16 @@
+import Chai from "./chai"
+
+function App() {
+  const userName = "chai aur code";
+  return (
+   <>
+   <Chai/>
+   <p>test para</p>
+   <h1>chai aur react {userName}</h1>
+   
+   </>
+    
+  )
+}
+
+export default App
---
+++ 01vitereact/src/main.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+function MyApp(){
+  return(
+    <div>
+      <h1>Custom App | chai</h1>
+    </div>
+  )
+}
+
+  const reactElement = {
+    type: 'a',
+    props: {
+        href: 'https://google.com',
+        target : '_blank'
+    },
+    children: 'Click me to visit google.com'
+  } 
+
+const ReactElement = React.createElement( 
+  'a', {href:"https://google.com", target:'_blank'}, 'click me to visit google'
+)
+
+const anotherElement = (<a href="https://google.com" target='_blank'>Visit google 12333</a>)
+
+
+ReactDOM.createRoot(document.getElementById('root')).
+render(
+  <App/>
+)
+ 
